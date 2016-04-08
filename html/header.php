9fe1d5c6c7c7416fd03bfed95685b3981fbe2150
Kameloh <jonmaltezo@gmail.com>
2016-04-08 12:20:10 -0700

    Added SSL

--- html/header.php
+++ html/header.php
@@ -5,8 +5,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="http://www.sketchbook.cafe/js/sketchbookcafe.js"></script>
-    <link rel="stylesheet" type="text/css" href="http://www.sketchbook.cafe/css/sketchbookcafe.css">
+    <script src="https://www.sketchbook.cafe/js/sketchbookcafe.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://www.sketchbook.cafe/css/sketchbookcafe.css">
 </head>
 <body>
 <!-- Page Wrap -->
--- html/index.php
+++ html/index.php
@@ -8,52 +8,5 @@ if ($address != '199.27.133.30')
     echo 'Under construction!';
     exit;
 }
-
 $App = new App();
-
 ?>
-
-<form name="test" method="post" action="http://www.sketchbook.cafe/home/testsubmit/">
-<input name="name" type="text">
-<input type="submit" value="Submit">
-</form>
-
-<?php
-
-/*
-// S3
-$s3 = new Aws\S3\S3Client([
-    'version' => 'latest',
-    'region'  => 'us-east-1'
-]);
-
-// Use the us-west-2 region and latest version of each client.
-$sharedConfig = [
-    'region'  => 'us-west-2',
-    'version' => 'latest'
-];
-
-// Create an SDK class used to share configuration across clients.
-$sdk = new Aws\Sdk($sharedConfig);
-
-// Create an Amazon S3 client using the shared configuration data.
-$client = $sdk->createS3();
-
-
-
-
-// Use an Aws\Sdk class to create the S3Client object.
-$s3Client = $sdk->createS3();
-
-
-// Download the contents of the object.
-$result = $s3Client->getObject([
-    'Bucket' => 'my-bucket',
-    'Key'    => 'my-key'
-]);
-
-// Print the body of the result by indexing into the result object.
-echo $result['Body'];
-
-echo 'hmm';
-*/
